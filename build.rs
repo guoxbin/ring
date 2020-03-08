0761c223b7221668375d6cc11695845f82494883
Bin Guo <guoxbin@gmail.com>
2020-03-08 05:02:29 -0700

    Enable win

--- build.rs
+++ build.rs
@@ -230,11 +230,11 @@ const LD_FLAGS: &[&str] = &[];
 const ASM_TARGETS: &[(&str, Option<&str>, &str)] = &[
     ("x86_64", Some("ios"), "macosx"),
     ("x86_64", Some("macos"), "macosx"),
-    //("x86_64", Some(WINDOWS), "nasm"),
+    ("x86_64", Some(WINDOWS), "nasm"),
     ("x86_64", None, "elf"),
     ("aarch64", Some("ios"), "ios64"),
     ("aarch64", None, "linux64"),
-    //("x86", Some(WINDOWS), "win32n"),
+    ("x86", Some(WINDOWS), "win32n"),
     ("x86", Some("ios"), "macosx"),
     ("x86", None, "elf"),
     ("arm", Some("ios"), "ios32"),
